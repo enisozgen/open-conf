6eda0256395e9cb4687616618698d0b5e8be5bfc
Enis OEZGEN <enisoezgen@amadeus.com>
2021-04-09 09:56:13 +0200

    Open Conf v1

---
+++ README.md
@@ -0,0 +1,30 @@
+# Open Conf
+
+`open-conf` allows you to open files quickly from M-x buffer with given alias.
+
+# Installation
+
+## MELPA
+
+  It will be in the Melpa in next days.
+
+  Just install the `open-conf` package!
+
+## Manual
+
+  Put `open-conf.el` in your load-path, and eval `(require 'open-conf')`. Than define your
+
+## use-package
+
+   Easy try with `use-package`
+
+``` elisp
+   (use-package open-conf
+     :ensure t
+     :config
+     (progn
+     (defun-open-conf  "bashrc"          "~/.bashrc")
+     (defun-open-conf  "zshrc"           "~/.zshrc")
+     (defun-open-conf  "etc-hosts-root"  "/sudo:root@localhost:/etc/hosts")
+     (defun-open-conf  "dnsmasq-root"    "/sudo:root@localhost:/etc/dnsmasq.conf")))
+```
---
+++ open-conf.el
@@ -0,0 +1,52 @@
+;;; open-conf.el --- Open file via M-x buffer
+
+;; Copyright (C) 2021 Enis OEZGEN
+
+;; Author: Enis OEZGEN <mail@enisozgen.com>
+;; Created: 08 Apr 2021
+;; Version: 1.0
+;; Keywords: convenience
+;; URL: https://github.com/enisozgen/open-conf
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;  Basically, if you don't touch Emacs `open-conf' will display your
+;;  org-agenda after certain time.
+;;  That can be useful to remember tasks after come back to work.
+;;  This project is comes from John Wiegley's mail at the gmane mailing list
+;;  http://article.gmane.org/gmane.emacs.orgmode/23047
+;;
+;; Please see README.md from the same repository for documentation.
+
+;;; Code:
+
+(defmacro defun-open-conf (config-name filepath)
+  "Open conf macro"
+  `(defun ,(intern (format "open-conf-%s" config-name)) ()
+     ,(format "Open %s: \"%s\"" config-name filepath)
+     (interactive)
+     (open-conf ,filepath)))
+
+(defun open-conf (filepath)
+  (switch-to-buffer
+   (find-file-noselect filepath)))
+
+
+(provide 'open-conf)
+
+;;; open-conf.el ends here
